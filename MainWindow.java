3cd26bf9b13ae96f4c6c20400dbb50df6467fcdf
Nate Fanning <motoxnate788@gmail.com>
2019-06-24 11:51:58 -0400

    Progress
    • Added + and – Button Actions

--- MainWindow.java
+++ MainWindow.java
@@ -10,7 +10,7 @@ import java.util.*;
 public class MainWindow {
     public  JPanel MainPanel;
     private JTabbedPane Tabs;
-    private JList displayedList;
+    private JList DatalogList;
     private JButton newFolderButton;
     private JComboBox SortingMethod;
     private JButton NewDatalog;
@@ -35,31 +35,72 @@ public class MainWindow {
     private List datalogs;
 
     public MainWindow() {
+        // Main Window Globals
         datalogs = new ArrayList();
         MainWindow self = this;
 
+        //Setup Buttons
+
         // TODO: Add all datalogs from libray before loading.
         datalogs.add(new Datalog("Test1"));
-        displayedList.setListData(datalogs.toArray());
-        displayedList.setCellRenderer(new ListRenderer());
+        DatalogList.setListData(datalogs.toArray());
+        DatalogList.setCellRenderer(new ListRenderer());
+
+        //New Datalog Button
         NewDatalogMain.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //Make new ask for datalog name window, which then adds it.
-                nameFrame = new JFrame("Datalog Name");
-                nameFrame.setContentPane(new DatalogName(self).panel);
-                nameFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-                nameFrame.pack();
-                nameFrame.setVisible(true);
+                newDatalogAction(self);
+            }
+
+        });
+
+        //New Datalog "+" Button
+        NewDatalog.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                newDatalogAction(self);
             }
         });
+
+        //Remove Datalog "–" Button
+        RemoveDatalog.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                removeDatalog();
+            }
+        });
+
+    }
+
+    protected void newDatalogAction(MainWindow self) {
+            //Make new ask for datalog name window, which then adds it.
+            nameFrame = new JFrame("Datalog Name");
+            nameFrame.setContentPane(new DatalogName(self).panel);
+            nameFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+            nameFrame.pack();
+            nameFrame.setVisible(true);
     }
 
     protected void addDatalog(Datalog log) {
         datalogs.add(log);
-        displayedList.setListData(datalogs.toArray());
+        updateDatalogs();
     }
 
+    protected void removeDatalog() {
+    //TODO Add Error Checking
+        try {
+            int index = DatalogList.getSelectedIndex();
+            datalogs.remove(index);
+            updateDatalogs();
+        } catch(Exception E) {
+            System.err.println("No Datalog to Remove!");
+        }
+    }
+
+    private void updateDatalogs() {
+        DatalogList.setListData(datalogs.toArray());
+    }
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
